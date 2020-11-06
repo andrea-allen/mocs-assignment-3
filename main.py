085c2532b3666c4b593ea98bc7c7b733c17972ad
Andrea Allen <andrea2allen@gmail.com>
2020-11-06 12:58:45 -0500

    first commit

---
+++ main.py
@@ -0,0 +1,10 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+from scipy import stats
+from matplotlib import rc
+
+if __name__ == '__main__':
+    rc('font', **{'family': 'sans-serif', 'sans-serif': ['Helvetica']})
+    rc('text', usetex=True)
+    print('Mocs Assignment 3\n By Mariah Boudreau, Amanda Bertschinger, Andrea Allen')
