e3bbb2fddb222f1d11463bd45e06bb167964fa3f
Andrea Allen <andrea2allen@gmail.com>
2020-11-11 11:14:22 -0500

    Fix theta function, clean up

--- integrators.py
+++ integrators.py
@@ -10,17 +10,17 @@ def diffy_q(I_k, N_k, k, p_k, lam, theta):
     return lam*k*(p_k-I_tilde_k)*theta - I_tilde_k
 
 def I_k_tilde(I_k, N_k):
-    return I_k/N_k
+    return I_k/200
 
 def euler(h, current_I_k, N_k, k, p_k, lam, theta):
     newY = current_I_k + h * diffy_q(current_I_k, N_k, k, p_k, lam, theta)
     return newY
 
-def get_theta(degree_dist, infected_vec):
+def get_theta(degree_dist, N_k, infected_vec):
     top_sum = 0
     bottom_sum = 0
     for k in range(len(degree_dist)):
-        top_sum+=(k*infected_vec[k])
+        top_sum+=(k*(infected_vec[k]/(np.sum(N_k))))
         bottom_sum+=(k*degree_dist[k])
     return top_sum/bottom_sum
 
